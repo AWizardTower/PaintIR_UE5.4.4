93532bc4709ef91c0ff22b6bb41b1932c7d15dd9
AWizardTower <3346581198@qq.com>
2025-04-03 15:19:22 +0800

    Blank Project

---
+++ Source/C_PaintIR.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class C_PaintIRTarget : TargetRules
+{
+	public C_PaintIRTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("C_PaintIR");
+	}
+}
